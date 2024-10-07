dff7b7675e9a832f99267e4ff8ca9df0b3bcad6a
Amaliebra <amaliebratland@gmail.com>
2024-10-07 20:42:15 +0200

    added some errors

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+
+public class WeatherData
+{
+    public Current Current { get; set; }
+}
+
+public class Current
+{
+    public double temperature_2m { get; set; }
+    public double Rain { get; set; }
+}
+
 
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
 string htmlPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
@@ -17,12 +31,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+
 app.UseStaticFiles();
 app.UseDefaultFiles();
 
-app.MapGet("/", () =>
+
+app.MapGet("/", async () =>
 {
-    return Results.Content(htmlContent, "text/html");
+    string url = $"https://api.open-meteo.com/v1/forecast?latitude=60.393&longitude=5.3242&current=temperature_2m,rain,weather_code&hourly=temperature_2m&forecast_days=3";
+    using (var httpClient = new HttpClient())
+    {
+        var response = await httpClient.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var weatherData = JsonSerializer.Deserialize<WeatherData>(responseString);
+            htmlContent = htmlContent.Replace("", weatherData.Current.temperature_2m.ToString());
+            htmlContent = htmlContent.Replace("", weatherData.Current.Rain.ToString());
+
+            return Results.Content(htmlContent, "text/html");
+        }
+        else
+        {
+            return Results.StatusCode((int)response.StatusCode);
+        }
+    }
 });
 
 app.Run();
