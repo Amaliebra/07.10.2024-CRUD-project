468454a9ab11aa667792f1911f326c8b834fb9f1
bax082024 <bax082024@gmail.com>
2024-10-08 13:16:54 +0200

    updated weatherdata.cs

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -1,6 +1,6 @@
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;  // Add this line
+using System.Text.Json.Serialization;  
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
